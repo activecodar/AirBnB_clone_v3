34ece6c3eb8c6f400be263aa6622870a38b2af6c
activecodar <activecodar@gmail.com>
2023-04-28 19:25:14 +0300

    CRUD endpoints for cities
    Signed-off-by: activecodar <activecodar@gmail.com>

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,6 +7,8 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 states_views = Blueprint('state_views', __name__, url_prefix='/api/v1')
+cities_views = Blueprint('cities_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *  # noqa
 from api.v1.views.states import *  # noqa
+from api.v1.views.cities import *  # noqa
