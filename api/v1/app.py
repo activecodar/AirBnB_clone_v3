adfeec1b108356b645407b8bd584c0dd1528ed85
activecodar <activecodar@gmail.com>
2023-04-28 13:14:18 +0300

    add a 404 error handling page
    Signed-off-by: activecodar <activecodar@gmail.com>

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,6 +25,17 @@ def teardown_appcontext(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handles all page not found 404 errors
+
+    Args:
+        error (Exception): The exception thrown in process
+    """
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=os.getenv('HBNB_API_PORT', 5000),
