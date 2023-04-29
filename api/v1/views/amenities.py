1ec1bc7fcca195d5f507a7dfd03bea3a5e3cb525
Denniskwg <dkamau476@gmail.com>
2023-04-29 12:56:52 +0300

    corrections to amenities view

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,6 +7,7 @@ from api.v1.views import amenities_views
 from models import storage
 from models.amenity import Amenity
 
+
 @amenities_views.route('/amenities',
                        methods=['GET'],
                        strict_slashes=False)
@@ -15,9 +16,10 @@ def get_all_amenities():
     amenities = storage.all(Amenity)
     return jsonify([item.to_dict() for item in amenities.values()])
 
+
 @amenities_views.route('/amenities/<amenity_id>',
-                        methods=['GET'],
-                        strict_slashes=False)
+                       methods=['GET'],
+                       strict_slashes=False)
 def get_amenity(amenity_id):
     """retrieves amenity related to amenity_id"""
     amenities = storage.all(Amenity)
@@ -25,6 +27,7 @@ def get_amenity(amenity_id):
         if amenity.id == escape(amenity_id):
             return jsonify(amenity.to_dict())
     abort(404)
+
 
 @amenities_views.route('/amenities/<amenity_id>',
                        methods=['DELETE'],
@@ -37,6 +40,7 @@ def delete(amenity_id):
     storage.delete(amenity)
     storage.save()
     return {}, 200
+
 
 @amenities_views.route('/amenities',
                        methods=['POST'],
@@ -54,6 +58,7 @@ def create():
         storage.save()
         return amenity.to_dict(), 201
 
+
 @amenities_views.route('/amenities/<amenity_id>',
                        methods=['PUT'],
                        strict_slashes=False)
@@ -66,10 +71,11 @@ def update(amenity_id):
     data = request.get_json(force=True, silent=True)
     if not data:
         return {"error": "Not a JSON"}, 400
+    if 'name' not in data.keys():
+        return {"error": "Missing name"}, 400
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             new_dict[key] = value
     amenity.__dict__.update(new_dict)
     storage.save()
     return amenity.to_dict(), 200
-
